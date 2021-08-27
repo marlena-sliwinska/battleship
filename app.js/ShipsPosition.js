194467780d355598e3c8cdc13a03b8dc3116a534
Marlena Sliwinska <marlenasliwinska@gmail.com>
2021-08-27 09:55:36 +0200

    finding possible next index method

--- app.js/ShipsPosition.js
+++ app.js/ShipsPosition.js
@@ -1,4 +1,5 @@
 import { game_panel_size } from "./Game.js";
+import { shipsTypes } from "./data/shipsTypes.js";
 class ShipsPosition {
   constructor() {}
 
@@ -19,6 +20,87 @@ class ShipsPosition {
       }
     }
     return data;
+  }
+
+  possibleNextIndex(x, y) {
+    let jakpot = [];
+    if (x == 0 && y == 0) {
+      jakpot = [
+        //right
+        { row: x + 1, column: y },
+        //bottom
+        { row: x, column: y + 1 },
+      ];
+    } else if (x == 0 && y == game_panel_size - 1) {
+      jakpot = [
+        //up
+        { row: x, column: y - 1 },
+        //right
+        { row: x + 1, column: y },
+      ];
+    } else if (x == game_panel_size - 1 && y == game_panel_size - 1) {
+      jakpot = [
+        //up
+        { row: x, column: y - 1 },
+        //left
+        { row: x - 1, column: y },
+      ];
+    } else if (x == game_panel_size - 1 && y == 0) {
+      jakpot = [
+        //bottom
+        { row: x, column: y + 1 },
+        //left
+        { row: x - 1, column: y },
+      ];
+    } else if (x == 0) {
+      jakpot = [
+        //up
+        { row: x, column: y - 1 },
+        //right
+        { row: x + 1, column: y },
+        //bottom
+        { row: x, column: y + 1 },
+      ];
+    } else if (y == game_panel_size - 1) {
+      jakpot = [
+        //up
+        { row: x, column: y - 1 },
+        //right
+        { row: x + 1, column: y },
+        //left
+        { row: x - 1, column: y },
+      ];
+    } else if (x == game_panel_size - 1) {
+      jakpot = [
+        //up
+        { row: x, column: y - 1 },
+        //bottom
+        { row: x, column: y + 1 },
+        //left
+        { row: x - 1, column: y },
+      ];
+    } else if (y == 0) {
+      jakpot = [
+        //right
+        { row: x + 1, column: y },
+        //bottom
+        { row: x, column: y + 1 },
+        //left
+        { row: x - 1, column: y },
+      ];
+    } else {
+      jakpot = [
+        //up
+        { row: x, column: y - 1 },
+        //right
+        { row: x + 1, column: y },
+        //bottom
+        { row: x, column: y + 1 },
+        //left
+        { row: x - 1, column: y },
+      ];
+    }
+    return jakpot;
   }
 }
 
