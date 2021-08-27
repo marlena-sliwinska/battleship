31f36b457112f9befb3f695d7439210e24621552
Marlena Sliwinska <marlenasliwinska@gmail.com>
2021-08-27 08:35:53 +0200

    ships types data

---
+++ app.js/data/shipsTypes.js
@@ -0,0 +1,12 @@
+export const shipsTypes = [
+  { name: "Destroyer", size: 4, id: "Destroyer_1" },
+  { name: "Gunboat", size: 3, id: "Gunboat_1" },
+  { name: "Gunboat", size: 3, id: "Gunboat_2" },
+  { name: "Submarine", size: 2, id: "Submarine_1" },
+  { name: "Submarine", size: 2, id: "Submarine_2" },
+  { name: "Submarine", size: 2, id: "Submarine_3" },
+  { name: "Patrol Boat", size: 1, id: "Patrol_Boat_1" },
+  { name: "Patrol Boat", size: 1, id: "Patrol_Boat_2" },
+  { name: "Patrol Boat", size: 1, id: "Patrol_Boat_3" },
+  { name: "Patrol Boat", size: 1, id: "Patrol_Boat_4" },
+];
