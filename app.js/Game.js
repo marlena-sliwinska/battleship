f314f8c639841b79fa06c584258fee18555ed0e8
Marlena Sliwinska <marlenasliwinska@gmail.com>
2021-08-27 13:54:06 +0200

    buttons gets , add even listener

--- app.js/Game.js
+++ app.js/Game.js
@@ -1,7 +1,13 @@
 import { field } from "./Field.js";
+
+import { game_panel_size } from "./ShipsPosition.js";
+
 import { shipsPosition } from "./ShipsPosition.js";
 
-export const game_panel_size = 10;
+import { stats } from "./Stats.js";
+
+import { game__single__field__className } from "./Field.js";
+
 const game_panel_id = "game__panel";
 
 class Game {
@@ -9,12 +15,15 @@ class Game {
     this.gameBoard = document.getElementById(game_panel_id);
     this.gameBoardData = [];
     this.initializeGame();
+    /*   console.log(this.gameBoardData); */
+    this.stats = stats;
   }
   initializeGame() {
     this.drawFieldsOnBoard();
     /* this.gameBoardData = shipsPosition.generateAllShipsLocation(); */
-    this.gameBoardData = shipsPosition.createTemplateDataTable();
-    console.log(this.gameBoardData);
+    this.gameBoardData = shipsPosition.initialGameBoardData;
+    /*  console.log(this.gameBoardData); */
+    this.getButtons();
   }
 
   drawFieldsOnBoard() {
@@ -24,6 +33,18 @@ class Game {
         this.gameBoard.appendChild(element);
       }
     }
+  }
+
+  getButtons() {
+    const buttons = document.getElementsByClassName(
+      game__single__field__className
+    );
+    for (let button of buttons) {
+      button.addEventListener("click", this.checkThisField);
+    }
+  }
+  checkThisField() {
+    console.log("test");
   }
 }
 
