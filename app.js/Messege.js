fd115de09ac5b2ab1f8697b21f08a6fb959e1e0b
Marlena Sliwinska <marlenasliwinska@gmail.com>
2021-08-28 13:45:45 +0200

    win Messege template

--- app.js/Messege.js
+++ app.js/Messege.js
@@ -1,37 +1,49 @@
-const messegee_Id = "result_messege";
+const messege_Id = "result_messege";
+const inactive_className = "inactive";
 const win_messege_content = "Congratulations! Victory is yours";
+const messegeText_className = "game__textMessage";
+const messegeQuote_className = "game__textMessage--quote";
+const messegeQuoteAuthor_className = "game__textMessage--author";
+
 import { win_quoutes } from "./data/quotes.js";
 
 class Messege {
   constructor() {
-    this.messegeBoard = document.getElementById(messegee_Id);
-    this.renderMessege();
+    this.messegeBoard = document.getElementById(messege_Id);
   }
   renderMessege() {
-    const messege = this.createQuoteMessege(win_quoutes);
-    console.log(messege);
-    /* this.messegeBoard.appendChild(messege); */
-    this.messegeBoard.innerHTML = `template string ${messege} <div>zabawmy sie</div>`;
-    /*  this.messegeBoard.insertAdjacentHTML("beforeend", `${messege}`); */
-    /*  this.messegeBoard.textContent = messege; */
+    this.messegeBoard.classList.remove(inactive_className);
+    const finalBoard = this.winMessege();
+    this.messegeBoard.appendChild(finalBoard);
   }
+
+  singleShipDestroyedMessege;
+
   winMessege() {
-    const quote = this.drawMessege(win_quoutes);
-    const quoteElement = document.createComment;
+    const winQuote = this.createQuoteMessege(win_quoutes);
+    const resultMessage = document.createElement("h2");
+    resultMessage.textContent = win_messege_content;
+    resultMessage.classList.add(messegeText_className);
+    const finalBoard = document.createElement("section");
+    finalBoard.appendChild(resultMessage, winQuote);
+    finalBoard.appendChild(resultMessage);
+    finalBoard.appendChild(winQuote);
+    return finalBoard;
   }
 
   destroyedShipMessege() {}
-  drawMessege(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
+
   createQuoteMessege(array) {
     const quote = array[Math.floor(Math.random() * array.length)];
     const quoteContentElement = document.createElement("p");
     quoteContentElement.textContent = quote.quote;
+    quoteContentElement.classList.add(messegeQuote_className);
+    quoteContentElement.classList.add(messegeText_className);
     const quoteAuthorElement = document.createElement("p");
     quoteAuthorElement.textContent = quote.author;
-    const quoteElement = document.createElement("div");
-    //zmien na blockquoute pozniej
+    quoteAuthorElement.classList.add(messegeText_className);
+    quoteAuthorElement.classList.add(messegeQuoteAuthor_className);
+    const quoteElement = document.createElement("article");
     quoteElement.appendChild(quoteContentElement);
     quoteElement.appendChild(quoteAuthorElement);
     return quoteElement;
